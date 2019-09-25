40eb09455f012c14b2600a67fd3310e02ecbdaee
Łukasz Antończyk <lukasz.antonczyk@fingo.pl>
2019-09-25 13:47:36 +0200

    Count left todos

--- lib/todo_web/views/todo_view.ex
+++ lib/todo_web/views/todo_view.ex
@@ -1,3 +1,15 @@
 defmodule TodoWeb.TodoView do
   use TodoWeb, :view
+
+  def left_todos(todos) do
+    left_todos = Enum.reject(todos, & &1.completed)
+    [
+      content_tag :strong do length(left_todos) end,
+      items(left_todos),
+      "left"
+    ] |> Enum.intersperse(" ")
+  end
+
+  defp items([_]), do: "item"
+  defp items(_), do: "items"
 end
