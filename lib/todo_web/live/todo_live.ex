361ea42eff65dfa23c468081819dc208edfc7c50
Łukasz Antończyk <lukasz.antonczyk@fingo.pl>
2019-09-25 12:54:58 +0200

    Adding todos

--- lib/todo_web/live/todo_live.ex
+++ lib/todo_web/live/todo_live.ex
@@ -1,11 +1,21 @@
 defmodule TodoWeb.TodoLive do
   use Phoenix.LiveView
 
+  @todos [
+    %Todo{title: "Taste Elixir & Phoenix LiveView", completed: true},
+    %Todo{title: "Buy a unicorn", completed: false}
+  ]
+
   def render(assigns) do
     TodoWeb.TodoView.render("index.html", assigns)
   end
 
   def mount(_, socket) do
-      {:ok, socket}
+    {:ok, assign(socket, :todos, @todos)}
+  end
+
+  def handle_event("add_todo", %{"todo" => %{"title" => title}}, socket) do
+    todo = %Todo{title: title}
+    {:noreply, assign(socket, :todos, socket.assigns.todos ++ [todo])}
   end
 end
