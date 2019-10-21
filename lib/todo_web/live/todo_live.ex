068f67d74d06c240618194161426c7e02c09ccbd
Łukasz Antończyk <lukasz.antonczyk@fingo.pl>
2019-10-21 17:32:45 +0200

    Integrate todo server with live view

--- lib/todo_web/live/todo_live.ex
+++ lib/todo_web/live/todo_live.ex
@@ -1,17 +1,16 @@
 defmodule TodoWeb.TodoLive do
   use Phoenix.LiveView
 
-  @todos [
-    %Todo{id: "1", title: "Taste Elixir & Phoenix LiveView", completed: true},
-    %Todo{id: "2", title: "Buy a unicorn", completed: false}
-  ]
+  alias Todo.TodoServer
 
   def render(assigns) do
     TodoWeb.TodoView.render("index.html", assigns)
   end
 
   def mount(%{todo_token: todos_id}, socket) do
-    {:ok, assign(socket, todos: @todos, filter: "all", todos_id: todos_id)}
+    TodoServer.start_link(todos_id)
+    todos = TodoServer.get(todos_id)
+    {:ok, assign(socket, todos: todos, filter: "all", todos_id: todos_id)}
   end
 
   def handle_params(%{"filter" => filter}, _uri, socket) when filter in ["active", "completed"] do
@@ -23,26 +22,14 @@ defmodule TodoWeb.TodoLive do
   end
 
   def handle_event("add_todo", %{"todo" => todo_params}, socket) do
-    case Todo.create(todo_params) do
-      {:ok, todo} ->
-        {:noreply, assign(socket, :todos, socket.assigns.todos ++ [todo])}
-      error ->
-        {:noreply, socket}
+    case TodoServer.add(socket.assigns.todos_id, todo_params) do
+      {:ok, todos} -> {:noreply, assign(socket, :todos, todos)}
+      error -> {:noreply, socket}
     end
   end
 
   def handle_event("toggle_todo", %{"id" => todo_id}, socket) do
-    todos = toggle_todo(socket.assigns.todos, todo_id)
+    todos = TodoServer.toggle(socket.assigns.todos_id, todo_id)
     {:noreply, assign(socket, :todos, todos)}
-  end
-
-  defp toggle_todo(todos, todo_id) do
-    Enum.map(todos, fn
-      %Todo{id: ^todo_id, completed: completed} = todo ->
-        %{todo | completed: !completed}
-
-      todo ->
-        todo
-    end)
   end
 end
