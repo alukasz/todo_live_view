acc272a4559f5dc5e769b5fd811b04453d422059
Łukasz Antończyk <lukasz.antonczyk@fingo.pl>
2019-10-21 17:20:38 +0200

    Filtering todos

--- lib/todo_web/router.ex
+++ lib/todo_web/router.ex
@@ -18,6 +18,7 @@ defmodule TodoWeb.Router do
     pipe_through :browser
 
     live "/", TodoLive, as: :todo
+    live "/:filter", TodoLive, as: :todo
   end
 
   # Other scopes may use custom stacks.
--- lib/todo_web/views/todo_view.ex
+++ lib/todo_web/views/todo_view.ex
@@ -1,3 +1,15 @@
 defmodule TodoWeb.TodoView do
   use TodoWeb, :view
+
+  def filter_todos(todos, "all"), do: todos
+  def filter_todos(todos, "active"), do: Enum.reject(todos, &(&1.completed))
+  def filter_todos(todos, "completed"), do: Enum.filter(todos, &(&1.completed))
+
+  def filter_link(socket, filter, active_filter) do
+    route = Routes.todo_path(socket, TodoWeb.TodoLive, filter)
+    live_link(String.capitalize(filter), to: route, class: filter_class(filter, active_filter))
+  end
+
+  defp filter_class(filter, filter), do: "selected"
+  defp filter_class(_, _), do: nil
 end
